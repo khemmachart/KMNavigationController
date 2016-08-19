a77a1ffaa44c93fcacbf8412682ef7ee8e943cba
9 <nueve.sr@gmail.com>
2016-08-19 17:17:17 +0700

    Add example class

---
+++ LearnNavigation/MyAISTableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MyAISTableViewController.swift
+//  LearnNavigation
+//
+//  Created by Khemmachart Chutapetch on 8/19/2559 BE.
+//  Copyright © 2559 Khemmachart Chutapetch. All rights reserved.
+//
+
+import Foundation
+
+class MyAISTableViewController1: BaseViewController {
+    
+}
+
+class MyAISTableViewController2: BaseViewController {
+    
+    override func viewDidAppear(animated: Bool) {
+        // self.myAISNavigationController?.showNavigaiton(animation: true)
+    }
+}
